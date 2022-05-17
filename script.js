b49faf29db69501833bfcb2e25d260302802a324
Gabriel Sousa <jgss.gabriel.sousa@gmail.com>
2022-05-17 18:17:04 -0300

    small improvements

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("header").style.top = "0";
-  } else {
-    document.getElementById("header").style.top = "-100px";
-  }
-  prevScrollpos = currentScrollPos;
+
+window.onscroll = () => {
+    var currentScrollPos = window.pageYOffset;
+    
+    if(prevScrollpos > currentScrollPos){
+        document.getElementById("header").style.top = "0";
+    }
+    else{
+        document.getElementById("header").style.top = "-100px";
+    }
+
+    prevScrollpos = currentScrollPos;
 }
