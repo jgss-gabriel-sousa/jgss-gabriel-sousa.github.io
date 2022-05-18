62aa9c8fa28c334a2e79adf3e54d3f27dc33cd17
Gabriel Sousa <jgss.gabriel.sousa@gmail.com>
2022-05-18 19:18:01 -0300

    project img and download link

--- scripts/projects.js
+++ scripts/projects.js
@@ -3,24 +3,40 @@ export function generateProjectsHTML(){
 
     projects.forEach(project => {
         projectCard.innerHTML += `
-            <div class="col">
-                <div class="project-card col card shadow-sm">
-                    <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+        <div class="col">
+            <div class="project-card col card shadow-sm">
+                <div class="bg-secondary m-0 rounded shadow-mx">
+                    `+imgHTML(project)+`
+                </div>
 
-                    <div class="card-body">
-                        <h3 class="card-text">${project.title}</h3>
-                        <p class="card-text">${project.description}</p>
-                        <div class="d-flex justify-content-between align-items-start">
-                            <div class="container d-flex flex-column align-items-start btn-group">
-                                <button type="button" class="btn btn-sm btn-outline-secondary mb-1"><a src="${project.github}">GitHub</a></button>
-                            </div>
-                            `+tagsHTML(project)+`
+                <div class="card-body">
+                    <h3 class="card-text">${project.title}</h3>
+                    <p class="card-text">${project.description}</p>
+                    <div class="d-flex justify-content-between align-items-start">
+                        <div class="container d-flex flex-column align-items-center btn-group">
+                            <a class="btn btn-sm btn-secondary mb-1" role="button" href="https://github.com/jgss-gabriel-sousa/${project.github}" target="_blank" rel="noopener noreferrer">GitHub</a>
+                            `+downloadBtnHTML(project)+`
                         </div>
+                        `+tagsHTML(project)+`
                     </div>
                 </div>
             </div>
+        </div>
         `;
     });
+}
+//<svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+
+function imgHTML(project){
+    if(project.img == "") 
+        return `
+        <figure class="mb-1">
+            <img class="contain rounded" width="100%" height="225" src="https://cataas.com/cat">
+            <figcaption class="small text-center text-light">Sem imagem disponível, mas fique com esse gatinho</figcaption>
+        </figure>
+        `;
+
+    return `<img class="contain rounded" width="100%" height="225" src="${project.img}">`;
 }
 
 function tagsHTML(project){
@@ -33,11 +49,19 @@ function tagsHTML(project){
     return tags;
 }
 
+function downloadBtnHTML(project){
+    if(project.download == "") return "";
+
+    return `<a class="btn btn-sm btn-secondary mb-1" role="button" href="${project.download}" target="_blank" rel="noopener noreferrer">Download</a>`;
+}
+
 const projects = [
     {
         title: "BEngine",
         description: "Game Engine em C++",
-        github: "google.com",
+        github: "BEngine",
+        download: "",
+        img: "",
         tags: [
             "C++",
             "SFML",
@@ -47,7 +71,9 @@ const projects = [
     {
         title: "BWNet Simulator",
         description: "Simulador de Redes sem Fio e Algoritmos de Roteamento",
-        github: "youtube.com",
+        github: "BWNet-Simulator",
+        download: "https://github.com/jgss-gabriel-sousa/BWNet-Simulator/releases/download/v2.1/BWNet.Simulator.v2.1.zip",
+        img: "https://camo.githubusercontent.com/6c20e9e550546c2a21ebcc1ceb3021cf919dc7bb2bf58c12e4b916e590e38da0/68747470733a2f2f692e696d6775722e636f6d2f5479425876556b2e706e67",
         tags: [
             "C++",
             "SFML",
