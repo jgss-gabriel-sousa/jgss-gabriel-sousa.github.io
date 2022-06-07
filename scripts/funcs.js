6279047580e5e28a27aac82aabf603a09a6616e4
Gabriel Sousa <jgss.gabriel.sousa@gmail.com>
2022-06-07 13:49:58 -0300

    new version

---
+++ scripts/funcs.js
@@ -0,0 +1,6 @@
+export function accentsTidy(s){
+    var r = s.toLowerCase();
+    const non_asciis = {'a': '[àáâãäå]', 'ae': 'æ', 'c': 'ç', 'e': '[èéêë]', 'i': '[ìíîï]', 'n': 'ñ', 'o': '[òóôõö]', 'oe': 'œ', 'u': '[ùúûűü]', 'y': '[ýÿ]'};
+    for (let i in non_asciis) { r = r.replace(new RegExp(non_asciis[i], 'g'), i); }
+    return r;
+};
--- scripts/projects.js
+++ scripts/projects.js
@@ -1,15 +1,25 @@
+import projectsData from "./projectsData.json" assert {type: 'json'};
+
 export function generateProjectsHTML(){
     let projectCard = document.getElementById("projects-cards");
     projectCard.innerHTML = "";
 
     const projects = projectsData;
+
+    const orderBy = document.getElementById("order-projects").value;
     
-    //sortArray(projects,"az");
+    sortProjects(projects, orderBy);
 
     projects.forEach(project => {
+        let classes = "";
+
+        project.tags.forEach(tags => {
+            classes += tags+" ";
+        });
+
         projectCard.innerHTML += `
-        <div class="col-11 d-flex">
-            <div class="project-card col card shadow-sm border-secondary">
+        <div class="project-card col-11 ${project.title} ${classes}">
+            <div class="col card shadow-sm border-secondary">
                 <div class="bg-secondary m-0 shadow-mx border-secondary">
                     `+imgHTML(project)+`
                 </div>
@@ -17,7 +27,7 @@ export function generateProjectsHTML(){
                 <div class="card-body">
                     <h3 class="card-text"><a class="link-dark animated-link" href="https://github.com/jgss-gabriel-sousa/${project.github}" target="_blank" rel="noopener noreferrer">${project.title}</a></h3>
                     <p class="card-text">${project.description}</p>
-                    <div class="d-flex justify-content-between align-items-start">
+                    <div class="project-link d-flex justify-content-between align-items-start">
                         <div class="container d-flex flex-column align-items-center btn-group">
                             `+downloadBtnHTML(project)+`
                             `+siteBtnHTML(project)+`
@@ -30,7 +40,6 @@ export function generateProjectsHTML(){
         `;
     });
 }
-//<svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
 
 function imgHTML(project){
     if(project.img == "") 
@@ -66,90 +75,11 @@ function siteBtnHTML(project){
     return `<a class="btn btn-sm btn-secondary mb-1 rounded" role="button" href="https://jgss-gabriel-sousa.github.io/${project.github}/" target="_blank" rel="noopener noreferrer">Acessar</a>`;
 }
 
-function sortArray(array, sortType){
+function sortProjects(array, sortType){
     if(sortType == "az")
         array.sort((a, b) => a.title.localeCompare(b.title));
     else if(sortType == "za")
         array.sort((a, b) => b.title.localeCompare(a.title));
+    else if(sortType == "popularity")
+        array.sort((a, b) => b.popularity - a.popularity);
 }
-
-const projectsData = [
-    {
-        title: "PokéDex",
-        description: "Site com uma PokéDex que obtém dados de API's externas",
-        github: "PokeDex",
-        download: "",
-        site: true,
-        img: "https://user-images.githubusercontent.com/42483024/169171404-3268f80e-e1e3-4892-a7de-f9e5b39c9318.jpg",
-        tags: [
-            "JavaScript",
-            "Front-End",
-            ""
-        ],
-    },
-    {
-        title: "Heroes and Monsters - Bestiário",
-        description: "Site com o Bestiário de um sistema de RPG de Mesa",
-        github: "Heroes-and-Monsters-Bestiario",
-        download: "",
-        site: true,
-        img: "https://user-images.githubusercontent.com/42483024/169554255-aa1382ab-a6bc-47e7-aef3-e9252d556484.png",
-        tags: [
-            "JavaScript",
-            "Front-End",
-            ""
-        ],
-    },
-    {
-        title: "BEngine",
-        description: "Game Engine em C++",
-        github: "BEngine",
-        download: "",
-        site: false,
-        img: "",
-        tags: [
-            "C++",
-            "SFML",
-            "GameDev"
-        ],
-    },
-    {
-        title: "BWNet Simulator",
-        description: "Simulador de Redes sem Fio e Algoritmos de Roteamento",
-        github: "BWNet-Simulator",
-        download: "https://github.com/jgss-gabriel-sousa/BWNet-Simulator/releases/download/v2.1/BWNet.Simulator.v2.1.zip",
-        site: false,
-        img: "https://camo.githubusercontent.com/6c20e9e550546c2a21ebcc1ceb3021cf919dc7bb2bf58c12e4b916e590e38da0/68747470733a2f2f692e696d6775722e636f6d2f5479425876556b2e706e67",
-        tags: [
-            "C++",
-            "SFML",
-            "GameDev"
-        ],
-    },
-    {
-        title: "Ano Novo",
-        description: "Contagem Regressiva para o Ano Novo",
-        github: "New-Year-Countdown",
-        download: "",
-        site: true,
-        img: "",
-        tags: [
-            "JavaScript",
-            "Front-End",
-            ""
-        ],
-    },
-    {
-        title: "Quiz Educativo",
-        description: "Site com um Quiz de Perguntas de temas variados",
-        github: "Quiz-Educativo",
-        download: "",
-        site: true,
-        img: "https://user-images.githubusercontent.com/42483024/169434528-79e0abd8-44bb-47ca-b7ef-12937b16be4b.png",
-        tags: [
-            "JavaScript",
-            "Front-End",
-            ""
-        ],
-    },
-]
