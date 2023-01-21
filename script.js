f77dc96eab3ee1871f1d43a574c874e7bec90d11
Gabriel Sousa <jgss.gabriel.sousa@gmail.com>
2023-01-21 00:40:13 -0300

    code improvement

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ import { generateProjectsHTML } from "./scripts/projects.js";
 import { addCats } from "./scripts/cats.js";
 import { filter } from "./scripts/filter.js";
 
-window.onscroll = () => hideNavbar();
 document.getElementById("order-projects").onchange = () => {generateProjectsHTML(); addCats();};
 document.getElementById("search-key").oninput = () => {filter();};
 document.getElementById("order-tag").onchange = () => {filter();};
 
 generateProjectsHTML();
 addCats();
+hideNavbar();
--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,15 +1,19 @@
-let prevScrollpos = window.pageYOffset;
-let currentScrollPos;
-
 export function hideNavbar(){
-    currentScrollPos = window.pageYOffset;
-    
-    if(prevScrollpos > currentScrollPos){
-        document.getElementById("header").style.top = "0";
-    }
-    else{
-        document.getElementById("header").style.top = "-100px";
-    }
+    const header = document.getElementById("header");
+    let prevScrollpos = window.pageYOffset;
+    let currentScrollPos;
 
     prevScrollpos = currentScrollPos;
+
+    window.addEventListener("scroll", () => {
+        currentScrollPos = window.pageYOffset;
+        
+        if(prevScrollpos > currentScrollPos) {
+            header.style.top = "0";
+        } else {
+            header.style.top = "-100px";
+        }
+
+        prevScrollpos = currentScrollPos;
+    });
 }
