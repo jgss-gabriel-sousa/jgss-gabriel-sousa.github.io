1e70fcd7f43523029ef93d46dfe442148fabd4b5
Gabriel Sousa <jgss.gabriel.sousa@gmail.com>
2023-03-05 22:18:51 -0300

    skills

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 import { hideNavbar } from "./scripts/navbar.js";
+import { generateSkillsHTML } from "./scripts/skills.js";
 import { generateProjectsHTML } from "./scripts/projects.js";
 import { addCats } from "./scripts/cats.js";
 import { filter } from "./scripts/filter.js";
@@ -7,6 +8,7 @@ document.getElementById("order-projects").onchange = () => {generateProjectsHTML
 document.getElementById("search-key").oninput = () => {filter();};
 document.getElementById("order-tag").onchange = () => {filter();};
 
+generateSkillsHTML();
 generateProjectsHTML();
 addCats();
 hideNavbar();
---
+++ scripts/skills.js
@@ -0,0 +1,21 @@
+import skillsData from "../data/skillsData.json" assert {type: 'json'};
+
+import { accentsTidy, blankSpaceFix } from "./funcs.js";
+
+export function generateSkillsHTML(){
+    let skillCard = document.getElementById("skills-cards");
+    skillCard.innerHTML = "";
+
+    const skills = skillsData;
+
+    skills.forEach(skill => {
+        skillCard.innerHTML += `
+        <div class="skills-card">
+            <div class="skills-card-body text-center">
+                <img class="mb-2" width="64px" src="img/icons/${blankSpaceFix(accentsTidy(skill))}.png">
+                <p class="card-text">${skill}</p>
+            </div>
+        </div>
+        `;
+    });
+}
